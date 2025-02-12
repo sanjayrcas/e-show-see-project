0dd3700e8e84a337dc46ee1dea6c2e3fd593d70d
sanjayrcas <sanjaygrcas@gmail.com>
2025-02-12 20:00:37 +0530

    app js was added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-// import React from 'react'
-// import Router from './components/Router'
+import React from 'react'
+import Router from './components/Router'
 
-// export default function App() {
-//   return (
-//     <div>
-//         <Router/>
-//     </div>
-//   )
-// }
-import React from "react";
-
-function App() {
-  console.log("API URL:", process.env.REACT_APP_API_URL);
-  console.log("Secret Key:", process.env.REACT_APP_SECRET_KEY);
-
+export default function App() {
   return (
     <div>
-      <h1>Environment Variables in React</h1>
-      <p>API URL: {process.env.REACT_APP_API_URL}</p>
+        <Router/>
     </div>
-  );
+  )
 }
 
-export default App;
