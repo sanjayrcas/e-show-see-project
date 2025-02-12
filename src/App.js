36b645fb77dade2618dae1b475b6a768d5174e16
sanjayrcas <sanjaygrcas@gmail.com>
2025-02-12 19:58:51 +0530

    Removed logo192.png and updated manifest.json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import Router from './components/Router'
+// import React from 'react'
+// import Router from './components/Router'
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <div>
+//         <Router/>
+//     </div>
+//   )
+// }
+import React from "react";
+
+function App() {
+  console.log("API URL:", process.env.REACT_APP_API_URL);
+  console.log("Secret Key:", process.env.REACT_APP_SECRET_KEY);
+
   return (
     <div>
-        <Router/>
+      <h1>Environment Variables in React</h1>
+      <p>API URL: {process.env.REACT_APP_API_URL}</p>
     </div>
-  )
+  );
 }
+
+export default App;
